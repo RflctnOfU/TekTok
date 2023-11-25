cba01e09c7ee69731d21687ed02d96441068b756
Kris Marshall <rflctnofu@hotmail.com>
2023-11-25 11:49:53 -0500

    commented out function

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -2,12 +2,12 @@ import { api } from "@/trpc/server";
 
 // import { User, Comment, Post } from "@/app/lib/definitions";
 
-export async function fetchAllPosts() {
-  try {
-    console.log("Fetching posts...");
-    return api.post.getPosts();
-  } catch (error) {
-    console.error("Database Error:", error);
-    throw new Error("Failed to fetch Posts");
-  }
-}
+// export async function fetchAllPosts() {
+//   try {
+//     console.log("Fetching posts...");
+//     return api.post.getPosts()
+//   } catch (error) {
+//     console.error("Database Error:", error);
+//     throw new Error("Failed to fetch Posts");
+//   }
+// }
